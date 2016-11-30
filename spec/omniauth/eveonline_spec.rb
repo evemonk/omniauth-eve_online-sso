2300802fa6c72790bf51d9977b8c76cf3774b40f
Igor Zubkov <igor.zubkov@gmail.com>
2016-11-30 22:40:20 +0200

    Cleanup

--- spec/omniauth/eveonline_spec.rb
+++ spec/omniauth/eveonline_spec.rb
@@ -4,8 +4,4 @@ describe Omniauth::Eveonline do
   it 'has a version number' do
     expect(Omniauth::Eveonline::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
