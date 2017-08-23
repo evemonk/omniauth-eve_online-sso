03edd29310277ba7231081b40c95a907a88a2f9f
Igor Zubkov <igor.zubkov@gmail.com>
2017-08-23 21:20:41 +0300

    Fix spec

--- spec/omniauth/eve_online/sso_spec.rb
+++ spec/omniauth/eve_online/sso_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe OmniAuth::Strategies::EveOnline::SSO do
+RSpec.describe OmniAuth::Strategies::EveOnlineSso do
   it 'has a version number' do
     expect(Omniauth::EveOnline::SSO::VERSION).not_to be nil
   end
