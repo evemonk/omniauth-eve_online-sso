a18c253ca6ea45ff7cb88218ae5f0efdb76e0e51
Igor Zubkov <igor.zubkov@gmail.com>
2021-08-15 16:19:00 +0300

    Upgrade to v2 (#2)

--- lib/omniauth/strategies/eve_online_sso.rb
+++ lib/omniauth/strategies/eve_online_sso.rb
@@ -7,7 +7,7 @@ module OmniAuth
     class EveOnlineSso < OmniAuth::Strategies::OAuth2
       option :name, "eve_online_sso"
 
-      option :client_options, authorize_path: "/oauth/authorize",
+      option :client_options, authorize_path: "/v2/oauth/authorize",
                               site: "https://login.eveonline.com/",
                               proxy: ENV["http_proxy"] ? URI(ENV["http_proxy"]) : nil
 
@@ -31,7 +31,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get("/oauth/verify").parsed
+        @raw_info ||= access_token.get("/v2/oauth/verify").parsed
       end
     end
   end
