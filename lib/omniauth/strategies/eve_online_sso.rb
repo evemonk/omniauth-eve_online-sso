a9fd1c4bb359f5412009b047a53597148ecd8ce9
Igor Zubkov <igor.zubkov@gmail.com>
2021-10-19 19:42:08 +0300

    Fix

--- lib/omniauth/strategies/eve_online_sso.rb
+++ lib/omniauth/strategies/eve_online_sso.rb
@@ -35,12 +35,10 @@ module OmniAuth
       def raw_info
         @raw_info ||= JWT.decode(access_token.token, nil, false)
           .find { |element| element.keys.include?("scp") }.tap do |hash|
-          hash[:character_id] = hash["sub"].split(":")[-1]
-          hash[:scopes] = hash["scp"].join(" ")
-          hash[:token_type] = hash["sub"].split(":")[0].capitalize
-          hash[:expires_on] = hash["exp"]
-          hash[:token] = access_token.token
-          hash[:refresh_token] = access_token.refresh_token
+          hash["character_id"] = hash["sub"].split(":")[-1]
+          hash["scopes"] = hash["scp"].join(" ")
+          hash["token_type"] = hash["sub"].split(":")[0].capitalize
+          hash["expires_on"] = hash["exp"]
         end
       end
     end
