ad3eaa78c687f6e6e38c2f57a379996c8ca93b3a
Igor Zubkov <igor.zubkov@gmail.com>
2026-07-18 16:21:42 +0200

    Fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "bundler/setup"
 require "simplecov"
 
 SimpleCov.start do
@@ -8,7 +9,6 @@ SimpleCov.start do
   primary_coverage :branch
 end
 
-require "bundler/setup"
 require "omniauth-eve_online-sso"
 
 RSpec.configure do |config|
